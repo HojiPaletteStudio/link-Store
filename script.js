45f5883d608e0eee396f4ac36ca072b5cd3855c3
Hoji Palette Studio <161171919+HojiPaletteStudio@users.noreply.github.com>
2024-07-02 18:41:22 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-function visitKofi() {
-    // เปิดลิงก์ Kofi ของคุณในแท็บใหม่
-    window.open("https://ko-fi.com/yourstream", "_blank");
-}
-function visitEtsy(){
-    window.open("https://www.etsy.com/shop/YourStream", "_blank");
-}
