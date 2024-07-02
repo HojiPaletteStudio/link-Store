bd1e8f755f63c0d53261dcae0944531145c25a93
Hoji Palette Studio <161171919+HojiPaletteStudio@users.noreply.github.com>
2024-07-02 18:22:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function visitKofi() {
+    // เปิดลิงก์ Kofi ของคุณในแท็บใหม่
+    window.open("https://ko-fi.com/yourstream", "_blank");
+}
+function visitEtsy(){
+    window.open("https://www.etsy.com/shop/YourStream", "_blank");
+}
